675080203e01a2381f399f264af6a654bd9cc96a
TegHocine <arcou98@hotmail.com>
2022-08-01 12:53:14 +0100

    basic project setup with vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/layout/Header'
+
+const App = () => {
+  return (
+    <div className='h-auto flex flex-col'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
