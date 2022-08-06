45da79f3b53e05913db77b351741c7676bcd1a61
TegHocine <arcou98@hotmail.com>
2022-08-06 21:29:31 +0100

    making the user info persistent with localstorage and dropdown menu for logout

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import CreateContainer from './components/layout/CreateContainer'
 
 const App = () => {
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <div className='h-auto flex flex-col bg-primary'>
         <Header />
         <main className='mt-24 p-8 w-full'>
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { useStateValue } from '../../context/StateProvider'
@@ -6,7 +6,7 @@ import { useStateValue } from '../../context/StateProvider'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { app } from '../../firebase'
 
-import { MdShoppingCart } from 'react-icons/md'
+import { MdShoppingCart, MdAdd, MdLogout } from 'react-icons/md'
 import { motion } from 'framer-motion'
 
 import logo from '../../assets/logo.png'
@@ -17,18 +17,28 @@ const Header = () => {
   const firebaseAuth = getAuth(app)
   const provider = new GoogleAuthProvider()
 
+  const [isMenu, setIsMenu] = useState(false)
+
   const [{ user }, dispatch] = useStateValue()
   const login = async () => {
-    const {
-      user: { refreshToken, providerData }
-    } = await signInWithPopup(firebaseAuth, provider)
-    dispatch({
-      type: actionType.SET_USER,
-      user: providerData[0]
-    })
+    if (!user) {
+      const {
+        user: { refreshToken, providerData }
+      } = await signInWithPopup(firebaseAuth, provider)
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0]
+      })
+
+      localStorage.setItem('user', JSON.stringify(providerData[0]))
+    }
+  }
+
+  const menu = () => {
+    setIsMenu(!isMenu)
   }
   return (
-    <header className='fixed top-0 z-50 py-6 px-16 w-full'>
+    <header className='fixed top-0 z-50  p-4 md:py-6 md:px-16 w-full'>
       {/* desktop */}
       <div className='hidden md:flex w-full h-full items-center justify-between'>
         <Link to='/' className='flex items-center gap-2'>
@@ -36,7 +46,11 @@ const Header = () => {
           <p className='text-headingColor text-xl font-bold'>City</p>
         </Link>
         <div className='flex items-center gap-8'>
-          <ul className='flex items-center gap-8'>
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className='flex items-center gap-8'>
             <li className='text-base hover:text-headingColor cursor-pointer text-textColor transition-all ease-in-out '>
               Home
             </li>
@@ -49,7 +63,7 @@ const Header = () => {
             <li className='text-base hover:text-headingColor cursor-pointer text-textColor transition-all ease-in-out '>
               Service
             </li>
-          </ul>
+          </motion.ul>
           {/* // Cart */}
           <div className='relative flex items-center justify-center'>
             <MdShoppingCart className='text-textColor text-2xl cursor-pointer' />
@@ -58,19 +72,41 @@ const Header = () => {
             </div>
           </div>
           {/* User Avatar */}
-          <div className='relative'>
+          <div className='relative' onClick={user ? menu : login}>
             <motion.img
               whileTap={{ scale: 0.6 }}
-              src={avatar}
+              src={user ? user.photoURL : avatar}
               alt='user profile'
-              className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer'
-              onClick={login}
+              className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full'
             />
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className='w-40 bg-gray-50 shadow-xl rounded-lg absolute flex flex-col top-12 right-0'>
+                {user && user.email === 'atlas.social.net@gmail.com' && (
+                  <Link to={'/createItem'}>
+                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all ease-in-out duration-100 text-textColor text-base'>
+                      New Item <MdAdd />
+                    </p>
+                  </Link>
+                )}
+                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all ease-in-out duration-100 text-textColor text-base'>
+                  Logout <MdLogout />
+                </p>
+              </motion.div>
+            )}
           </div>
         </div>
       </div>
       {/* Mobile */}
-      <div className='flex md:hidden '></div>
+      <div className='flex md:hidden '>
+        <Link to='/' className='flex items-center gap-2'>
+          <img src={logo} alt='logo' className='w-8 object-cover' />
+          <p className='text-headingColor text-xl font-bold'>City</p>
+        </Link>
+      </div>
     </header>
   )
 }
