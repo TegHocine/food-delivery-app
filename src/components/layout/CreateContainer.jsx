7d5ef987e5606546eb98e12358fcb2b9e85a2ec4
TegHocine <arcou98@hotmail.com>
2022-08-09 12:04:38 +0100

    createContainer(create item) comp image upload input

--- src/components/layout/CreateContainer.jsx
+++ src/components/layout/CreateContainer.jsx
@@ -23,6 +23,14 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState('danger')
   const [msg, setMsg] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
+
+  const uploadImage = () => {
+    console.log('upload')
+  }
+  const deleteImage = () => {
+    console.log('delete')
+  }
+
   return (
     <div className='w-full h-full flex items-center justify-center'>
       <div className='w-[90%] md:w-[50%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4'>
@@ -72,7 +80,48 @@ const CreateContainer = () => {
         </div>
         {/* item image */}
         <div className='group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg'>
-          {isLoading ? <Loader /> : <></>}
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {!imageAsset ? (
+                <>
+                  <label className='w-full h-full flex flex-col items-center justify-center cursor-pointer'>
+                    <div className='w-full h-full flex flex-col items-center justify-center gap-2'>
+                      <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700' />
+                      <p className='text-gray-500 hover:text-gray-700'>
+                        Click here to upload
+                      </p>
+                    </div>
+                    <input
+                      type='file'
+                      name='uploadimage'
+                      accept='image/*'
+                      onChange={uploadImage}
+                      className='w-0 h-0'
+                    />
+                  </label>
+                </>
+              ) : (
+                <>
+                  {' '}
+                  <div className=' relative h-full '>
+                    <img
+                      src={imageAsset}
+                      alt='uploaded image'
+                      className='w-full h-full object-cover'
+                    />
+                    <button
+                      type='button'
+                      className='absolute bottom-3 right-3 p-3 rounded-full text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out '
+                      onClick={deleteImage}>
+                      <MdDelete className='text-white' />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          )}
         </div>
       </div>
     </div>
